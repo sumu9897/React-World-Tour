61380bf5673584185a1d87a8fb35902faa70ed27
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-19 21:04:59 +0600

    API connect

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
+    const [countries, setCountries]= useState([]);
+
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    })
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries: {countries.length}</h3>
+            {
+                countries.map(country =><Country country = {country}></Country>)
+            }
         </div>
     );
 };
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+
+
+const Country = ({country}) => {
+    const {name} =country;
+
+    return (
+        <div>
+            <h3>Name : {name.common}</h3>
+        </div>
+    );
+};
+
+export default Country;
