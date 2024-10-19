ce99ae3eb55ac3af4435da274219684a63f61352
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-19 21:15:24 +0600

    Small Changes

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,12 @@
-
+import './Country.css';
 
 const Country = ({country}) => {
-    const {name} =country;
+    const {name,flags} =country;
 
     return (
-        <div>
-            <h3>Name : {name.common}</h3>
+        <div className='country'>
+            <h3>Country Name : {name?.common}</h3>
+            <img src={flags.png} alt={name?.common} />
         </div>
     );
 };
